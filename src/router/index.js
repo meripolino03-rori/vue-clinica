fda72d8206c512730c7d3df6535f073d54ccccd0
meripolino03-rori <meripolino03@gmail.com>
2026-08-03 23:00:40 -0500

    Configurar GitHub Router

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior() {
     return { top: 0, behavior: 'smooth' }
